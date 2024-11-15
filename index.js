f0a10dc33a57889d2d33082261a5666038c528ea
Rohit Masani <rohitmasani373@gmail.com>
2024-11-15 14:08:35 +0530

    This is my first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { response, request } = require('express');
+
+express = require('express');
+app = express();
+
+
+app.use(express.json());
+
+
+
+app.get('/', function (req, res) {
+    res.send("hello word")
+});
+
+app.get('/morning', function (req, res) {
+    const mgs =req.query.notice;
+    console.log(msg)
+    response.send("responce from sarver:"+mgs);
+});
+
+app.get('/night', function (req, res) {
+    res.send(" good night sir")
+});
+
+app.post('/evning',function ( req,res){
+    console.log (request.body.notice);
+    res.send(" this is responce for post");
+});
+
+
+app.listen(3000, () => {
+    console.log("server started on port 3000.");
+});
